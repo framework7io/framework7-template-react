621cb747dd4bff7de0729cff0195065c0c84d0f9
bc022699 <Ben.Compton@cerner.com>
2017-02-20 14:04:16 -0500

    Add app starter css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import 'framework7/dist/css/framework7.ios.min.css'
-import 'framework7/dist/css/framework7.ios.colors.min.css'
+import 'framework7/dist/css/framework7.ios.min.css';
+import 'framework7/dist/css/framework7.ios.colors.min.css';
 
 /* OR for Material Theme:
 import 'framework7/dist/css/framework7.material.min.css'
 import 'framework7/dist/css/framework7.material.colors.min.css'
 */
+
+import './css/app.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
