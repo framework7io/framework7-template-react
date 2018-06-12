8373de2ec9cca2ef052db2a1bc53ab217f6f39e1
Ben Compton <benjamincompton@gmail.com>
2018-06-12 14:19:04 -0400

    Fix dynamic route page bugs

--- src/components/pages/DynamicRoutePage.jsx
+++ src/components/pages/DynamicRoutePage.jsx
@@ -8,28 +8,27 @@ export default class DynamicRoutePage extends Component {
         <Navbar title="Dynamic Route" backLink="Back" />
         <Block strong>
           <ul>
-            <li><b>Url:</b> {this.$f7route.url}</li>
-            <li><b>Path:</b> {this.$f7route.path}</li>
-            <li><b>Hash:</b> {this.$f7route.hash}</li>
+            <li><b>Url:</b> {this.props.f7route.url}</li>
+            <li><b>Path:</b> {this.props.f7route.path}</li>
             <li><b>Params:</b>
               <ul>
-                {Object.keys(this.$f7route.params).map(key => (
-                  <li key={key}><b>{key}:</b> {this.$f7route.params[key]}</li>
+                {Object.keys(this.props.f7route.params).map(key => (
+                  <li key={key}><b>{key}:</b> {this.props.f7route.params[key]}</li>
                 ))}            
               </ul>
             </li>
             <li><b>Query:</b>
               <ul>
                 {Object.keys(this.$f7route.query).map(key => (
-                  <li key={key}><b>{key}:</b> {this.$f7route.query[key]}</li>
+                  <li key={key}><b>{key}:</b> {this.props.f7route.query[key]}</li>
                 ))}
               </ul>
             </li>
-            <li><b>Route:</b> {this.$f7route.route}</li>
+            <li><b>Route:</b> {JSON.stringify(this.props.f7route.route)}</li>
           </ul>
         </Block>
         <Block strong>
-          <Link onClick={this.$f7router.back()}>Go back via Router API</Link>
+          <Link onClick={() => this.props.f7router.back()}>Go back via Router API</Link>
         </Block>
       </Page>
     );
