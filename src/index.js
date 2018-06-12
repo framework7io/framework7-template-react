8d393fdf235facf3ba9d26a5ec71cbb2f9769d28
Ben Compton <benjamincompton@gmail.com>
2018-06-12 14:20:30 -0400

    Improve ability to debug

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/cjs/react-dom.production.min';
+import ReactDOM from 'react-dom';
 import Framework7 from 'framework7/framework7.esm.bundle';
 import Framework7React from 'framework7-react';
 
 import App from './components/App.jsx';
 
 import 'framework7/css/framework7.min.css';
+import './css/app.css';
+import './css/icons.css';
 
 Framework7.use(Framework7React);
 
